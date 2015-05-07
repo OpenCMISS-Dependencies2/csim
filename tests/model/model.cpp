8f442511d4ac5a54a92e6bf579a80392d246a427
David Nickerson <nickerso@users.sourceforge.net>
2015-05-07 16:59:56 +1200

    adding a few more CellML tests and corresponding resources; invalid model test fails since we are not yet checking for models that can be executed

--- tests/model/model.cpp
+++ tests/model/model.cpp
@@ -17,9 +17,27 @@ TEST(Model, load_nonexisting_model) {
               model.loadCellmlModel("http://example.com/this.cellml.file.should.never.exist.xml"));
 }
 
-TEST(Model, load_noncellml_model) {
+TEST(Model, load_non_cellml_model) {
     csim::Model model;
     EXPECT_EQ(csim::UNABLE_TO_LOAD_MODEL_URL,
               model.loadCellmlModel(TestResources::getLocation(TestResources::SBML_MODEL_RESOURCE)));
+}
+
+TEST(Model, load_invalid_cellml_model) {
+    csim::Model model;
+    EXPECT_EQ(csim::UNABLE_TO_LOAD_MODEL_URL,
+              model.loadCellmlModel(TestResources::getLocation(TestResources::CELLML_INVALID_MODEL_RESOURCE)));
+}
+
+TEST(Model, load_valid_cellml_model) {
+    csim::Model model;
+    EXPECT_EQ(csim::CSIM_OK,
+              model.loadCellmlModel(TestResources::getLocation(TestResources::CELLML_SINE_MODEL_RESOURCE)));
+}
+
+TEST(Model, load_valid_cellml_model_with_imports) {
+    csim::Model model;
+    EXPECT_EQ(csim::CSIM_OK,
+              model.loadCellmlModel(TestResources::getLocation(TestResources::CELLML_SINE_IMPORTS_MODEL_RESOURCE)));
 }
 
--- tests/model/tests.cmake
+++ tests/model/tests.cmake
@@ -7,4 +7,8 @@ set(${CURRENT_TEST}_SRCS
 )
 
 set(SBML_MODEL_RESOURCE "${CMAKE_CURRENT_SOURCE_DIR}/resources/BIOMD0000000020.xml")
+set(CELLML_SINE_MODEL_RESOURCE "${CMAKE_CURRENT_SOURCE_DIR}/resources/sine/sin_approximations.xml")
+set(CELLML_SINE_IMPORTS_MODEL_RESOURCE "${CMAKE_CURRENT_SOURCE_DIR}/resources/sine/sin_approximations_import.xml")
+set(CELLML_INVALID_MODEL_RESOURCE "${CMAKE_CURRENT_SOURCE_DIR}/resources/invalid_cellml_1.0.xml")
+set(CELLML_UNDERCONSTRAINED_MODEL_RESOURCE "${CMAKE_CURRENT_SOURCE_DIR}/resources/underconstrained_cellml_1.0.xml")
 
