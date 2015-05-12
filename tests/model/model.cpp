3655dbe0ebb89d91a359355f232e4a11230aec96
David Nickerson <nickerso@users.sourceforge.net>
2015-05-12 12:14:40 +1200

    adding some more tests to better ensure things are working correctly

--- tests/model/model.cpp
+++ tests/model/model.cpp
@@ -51,4 +51,24 @@ TEST(Model, set_valid_io_variables) {
     EXPECT_EQ(2, model.setVariableAsOutput("main/sin3"));
     EXPECT_EQ(2, model.setVariableAsOutput("main/sin3"));
     EXPECT_EQ(3, model.setVariableAsOutput("main/deriv_approx_initial_value"));
+    EXPECT_EQ(csim::CSIM_OK,
+              model.loadCellmlModel(TestResources::getLocation(TestResources::CELLML_SINE_MODEL_RESOURCE)));
+    EXPECT_EQ(0, model.setVariableAsOutput("actual_sin/sin"));
+    EXPECT_EQ(1, model.setVariableAsOutput("deriv_approx_sin/sin"));
+    // check that source variables are correctly resolved
+    EXPECT_EQ(0, model.setVariableAsOutput("main/sin1"));
+    EXPECT_EQ(1, model.setVariableAsOutput("main/sin2"));
+    EXPECT_EQ(2, model.setVariableAsOutput("main/sin3"));
 }
+
+TEST(Model, set_invalid_io_variables) {
+    csim::Model model;
+    EXPECT_EQ(csim::CSIM_OK,
+              model.loadCellmlModel(TestResources::getLocation(TestResources::CELLML_SINE_MODEL_RESOURCE)));
+    EXPECT_EQ(csim::UNABLE_TO_FLAG_VARIABLE, model.setVariableAsInput("main/z"));
+    EXPECT_EQ(csim::UNABLE_TO_FLAG_VARIABLE, model.setVariableAsOutput("main/xx"));
+    EXPECT_EQ(0, model.setVariableAsOutput("deriv_approx_sin/sin"));
+    EXPECT_EQ(csim::UNABLE_TO_FLAG_VARIABLE, model.setVariableAsOutput("main/xx"));
+    EXPECT_EQ(csim::MISMATCHED_COMPUTATION_TARGET, model.setVariableAsInput("deriv_approx_sin/sin"));
+}
+
