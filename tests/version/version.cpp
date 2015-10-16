bbc4a6bd119e17a457912a7d0db45e2372a04468
David Nickerson <nickerso@users.sourceforge.net>
2015-10-15 18:50:46 -0600

    update version test to match new version

--- tests/version/version.cpp
+++ tests/version/version.cpp
@@ -4,9 +4,9 @@
 
 TEST(Version, version_match) {
     unsigned int version = csim::version();
-    EXPECT_EQ(0x020000, version);
+    EXPECT_EQ(0x020001, version);
 
     std::string version_string = csim::versionString();
-    EXPECT_EQ("2.0.0", version_string);
+    EXPECT_EQ("2.0.1", version_string);
 }
 
