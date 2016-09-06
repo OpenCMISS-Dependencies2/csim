9b9c22385977bc3db3061171bec85cccdbb217be
David Nickerson <nickerso@users.sourceforge.net>
2016-09-07 11:38:26 +1200

    Need to bump the version number in the tests

--- tests/version/version.cpp
+++ tests/version/version.cpp
@@ -4,9 +4,9 @@
 
 TEST(Version, version_match) {
     unsigned int version = csim::version();
-    EXPECT_EQ(0x020001, version);
+    EXPECT_EQ(0x020002, version);
 
     std::string version_string = csim::versionString();
-    EXPECT_EQ("2.0.1", version_string);
+    EXPECT_EQ("2.0.2", version_string);
 }
 
